def1deef8ec932683cfa1c6dcd57cd09e35c926e
Swanand@143 <swanandgholap375@gmail.com>
2024-07-26 19:05:23 +0530

    cards added

--- src/component/Cake-card/Card.js
+++ src/component/Cake-card/Card.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import config from './../../config.json'
+import './Card.css'
 
-function Card() {
+function Card({id, title, previewDescription, detailDescription, image, price }) {
+  const headingStyle = {
+    color: config.themeColors.headingColor
+  };
+  const cardbodycolor = {
+    backgroundColor: config.themeColors.cakeCardbg
+  };
   return (
-    <div>Card</div>
-  )
+    <div className="card" style={cardbodycolor} key={id}>
+      <div className="card-body">
+        <div className="card-image-container">
+        <img className="card-image" src={image} alt={title} />
+        </div>
+        <h3
+          className="card-title"
+          style={headingStyle}>
+          {title}
+        </h3>
+        <p className="card-description"> {previewDescription} </p>
+        <p className="card-text"> Price: ₹{price} </p>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import config from "./../../config.json";
+import Card from "../../component/Cake-card/Card";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div style={{backgroundColor : config.themeColors.bodybg}} className="container p-4 ">
+      <h1
+        className="text-center"
+        style={{ color: config.themeColors.headingColor }}
+      >
+        {config.product}
+      </h1>
+      {/* <p className="text-center"> {config.} </p> */}
+      <h3 className="text-center"> services </h3>
+      <div className="card-container">
+        {config.cakes.map((serviceData, index) => {
+          const {
+            id,
+            title,
+            previewDescription,
+            detailDescription,
+            image,
+            price,
+          } = serviceData;
+          return (
+            <Card
+              key={index}
+              id={id}
+              title={title}
+              previewDescription={previewDescription}
+              detailDescription={detailDescription}
+              image={image}
+              price={price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
